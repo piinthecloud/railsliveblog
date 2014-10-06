f3d855dbff0705a5ee3173fbf69aed5526cb6257
stephanie pi <stephmpi@gmail.com>
2014-10-06 16:39:31 -0700

    make new post form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,20 @@
 class PostsController < ApplicationController
 
   def index
+    @posts = Post.all
 
+  end
+
+  def new
+    @posts = Post.new
+  end
+
+  def create
+    @post = Post.new(params.require(:post).permit(:title, :content))
+    if @post.save
+      redirect_to root_path
+    else
+    end
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
 
 
   get "/posts", to: "posts#index"
+  post "/posts",     to: "posts#create"
+  get "/posts/new", to: "posts#new"
+
+  root "posts#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
